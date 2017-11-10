116ba6a9d2766bcc3d2b10a409e97a9e1f38d2c4
Tanay Vardhan <tvardha2@illinois.edu>
2017-11-10 17:23:55 -0600

    Added express to package -_-

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ app.post('/webhook', (req, res) => {
 app.get('/webhook', (req, res) => {
     
     // Your verify token. Should be a random string.
-    let VERIFY_TOKEN = "<YOUR_VERIFY_TOKEN>"
+    let VERIFY_TOKEN = "TING_GOES_SKRRAA"
     
     // Parse the query params
     let mode = req.query['hub.mode'];
